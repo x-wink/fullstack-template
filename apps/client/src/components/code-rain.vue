e746fca8027264756d9b02ec85f64be7597ba824
向文可 <13202090601@163.com>
2023-05-24 18:55:25 +0800

    feat: layout

--- apps/client/src/components/code-rain.vue
+++ apps/client/src/components/code-rain.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 组件效果会根据canvas的样式进行变化，如背景颜色，字体，颜色 -->
-    <canvas ref="refsCanvas" class="x-code-rain"></canvas>
+    <canvas ref="refsCanvas" class="x-code-rain" :class="{ '--background': props.background }"></canvas>
 </template>
 
 <script setup lang="ts">
@@ -20,11 +20,20 @@
              * 密集程度，数字越小下落越密集
              */
             density?: number;
+            /**
+             * 是否作为背景
+             */
+            background?: boolean;
+            /**
+             * 雨滴长度
+             */
+            length?: number;
         }>(),
         {
             charset: 'qwertyuiopasdfghjklzxcvbnm0123456789',
-            step: 10,
-            density: 20,
+            step: 100,
+            density: 15,
+            length: 15,
         }
     );
     const refsCanvas = ref<HTMLCanvasElement>();
@@ -38,16 +47,15 @@
         cvs.width = cvs.clientWidth * ratio;
         cvs.height = cvs.clientHeight * ratio;
         const style = getComputedStyle(cvs);
-        // 限制背景颜色透明度，不然没有渐隐效果
-        const [r, g, b, a = '1'] = style.backgroundColor.match(/[\d\\.]+/g)!;
-        const bgc = `rgba(${r},${g},${b},${Math.min(0.067, +a)})`;
         ctx.font = style.font;
-        const fontSize = Number(style.fontSize.replace('px', ''));
+        const fontSize = Number(style.fontSize.replace('px', '')) * 1.2;
         const cols = ~~(cvs.width / fontSize);
-        return { cvs, ctx, fontSize, backgroundColor: bgc, color: style.color, cols };
+        return { cvs, ctx, fontSize, backgroundColor: style.backgroundColor, color: style.color, cols };
     };
-    const random = (min: number, max: number) => Math.round(Math.random() * (max - min) + min);
+    const randomInt = (min: number, max: number) => Math.round(Math.random() * (max - min) + min);
+    const randomChar = () => props.charset[randomInt(0, props.charset.length - 1)];
     let cancelId = 0;
+    let data = [] as string[][];
     let position = [] as number[];
     onMounted(() => {
         let { cvs, ctx, backgroundColor, color, fontSize, cols } = init();
@@ -57,29 +65,46 @@
             color = res.color;
             fontSize = res.fontSize;
             cols = res.cols;
-            while (position.length < cols) {
-                position.push(0);
+            while (data.length < cols) {
+                data.push([]);
+                position.push(randomInt(-props.length, 0));
             }
             window.addEventListener('resize', update, { once: true });
         };
         window.addEventListener('resize', update, { once: true });
-        position = Array.from({ length: cols }, () => random(0, 100));
+        data = Array.from({ length: cols }, () =>
+            Array.from({ length: randomInt(0, props.length) }, () => randomChar())
+        );
+        position = Array.from({ length: cols }, () => randomInt(-props.length, props.length));
         let lasttime = 0;
         const render = (time = 0) => {
             cancelId = requestAnimationFrame(render);
             if (time - lasttime > props.step) {
                 lasttime = time;
+
+                ctx.clearRect(0, 0, cvs.width, cvs.height);
                 ctx.fillStyle = backgroundColor;
                 ctx.fillRect(0, 0, cvs.width, cvs.height);
+
                 ctx.fillStyle = color;
+                ctx.textAlign = 'center';
                 for (let i = 0; i < cols; i++) {
                     position[i]++;
-                    const char = props.charset[random(0, props.charset.length - 1)];
-                    const x = i * fontSize,
-                        y = position[i] * fontSize;
-                    ctx.fillText(char, x, y);
-                    if (y > cvs.height && random(1, props.density) > props.density - 1) {
-                        position[i] = 0;
+                    const col = data[i];
+                    if (col.length === props.length) {
+                        col.shift();
+                    }
+                    const char = props.charset[randomInt(0, props.charset.length - 1)];
+                    data[i].push(char);
+                    for (let offset = 0, len = data[i].length; offset < len; offset++) {
+                        ctx.globalAlpha = offset * (1 / props.length);
+                        const x = i * fontSize,
+                            y = (position[i] + offset) * fontSize;
+                        ctx.fillText(data[i][offset], x, y);
+                    }
+                    if (position[i] * fontSize > cvs.height && randomInt(1, props.density) > props.density / 2) {
+                        data[i] = Array.from({ length: randomInt(0, props.length) }, () => randomChar());
+                        position[i] = randomInt(-data[i].length, 0);
                     }
                 }
             }
@@ -98,6 +123,13 @@
         font-size: 1em;
         font-family: '微软雅黑';
         color: #0f0;
-        background-color: #00000011;
+        background-color: #000000;
+        &.--background {
+            pointer-events: none;
+            position: absolute;
+            z-index: 0;
+            left: 0;
+            top: 0;
+        }
     }
 </style>
--- apps/client/src/views/home.vue
+++ apps/client/src/views/home.vue
@@ -1,16 +1,7 @@
 <template>
-    <div class="container">
-        <code-rain style="font-size: 2em" />
-    </div>
+    <div>home</div>
 </template>
 
-<script setup lang="ts">
-    import CodeRain from '@/components/code-rain.vue';
-</script>
+<script setup lang="ts"></script>
 
-<style scoped lang="less">
-    .container {
-        width: 100%;
-        height: 100%;
-    }
-</style>
+<style scoped lang="less"></style>
