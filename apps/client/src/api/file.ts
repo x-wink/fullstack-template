7e4cbdb2bdedf6c499e7fb49ca31e0ecbaa433e7
向文可 <13202090601@163.com>
2023-05-26 18:45:56 +0800

    chore: 删除示例代码

--- apps/client/src/api/file.ts
+++ apps/client/src/api/file.ts
@@ -1,7 +1,6 @@
 import { post } from './request';
 export const uploadFile = (file: File) => {
     const data = new FormData();
-    data.append('id', JSON.parse(localStorage.getItem('user') || '{}').id || '0');
     data.append('file', file);
     return post<string>('/file/upload', data);
 };
