406574b5759a873cda8d82329e83cb737e8c9794
向文可 <13202090601@163.com>
2023-06-09 09:53:46 +0800

    feat: init sql

--- apps/server/dao/index.ts
+++ apps/server/dao/index.ts
@@ -8,10 +8,45 @@ const dao = useDao({
     logger,
     initSql: [
         `CREATE TABLE if not exists user (
-        id int(5) primary key auto_increment not null,
-        del_flag tinyint(1) not null default 0,
-        photo varchar(64)
-    )`,
+            id int(5) primary key auto_increment not null,
+            del_flag tinyint(1) not null default 0,
+            username varchar(20) not null,
+            nickname varchar(20),
+            password varchar(64) not null,
+            phone varchar(20),
+            photo varchar(64),
+            enabled tinyint(1) not null default 1,
+            locked tinyint(1) not null default 0,
+            expired tinyint(1) not null default 0
+        )`,
+        `CREATE TABLE if not exists role (
+            id int(5) primary key auto_increment not null,
+            del_flag tinyint(1) not null default 0,
+            name varchar(20) not null,
+            code varchar(20) not null,
+            enabled tinyint(1) not null default 1
+        )`,
+        `CREATE TABLE if not exists menu (
+            id int(5) primary key auto_increment not null,
+            del_flag tinyint(1) not null default 0,
+            parent_id int(5),
+            permission varchar(20) not null,
+            label varchar(20) not null,
+            sort int(5) not null default 0,
+            url varchar(64) not null,
+            type int(5) not null default 0,
+            visible tinyint(1) not null default 1
+        )`,
+        `CREATE TABLE if not exists r_user_role (
+            user_id int(5) not null,
+            role_id int(5) not null,
+            primary key(user_id, role_id)
+        )`,
+        `CREATE TABLE if not exists r_role_menu (
+            role_id int(5) not null,
+            menu_id int(5) not null,
+            primary key(role_id, menu_id)
+        )`,
     ],
 });
 export const get = dao.get;
--- apps/server/dao/user.ts
+++ apps/server/dao/user.ts
@@ -1,18 +1,18 @@
 import { get, insert, remove, select, update } from './';
 import { User } from '../entity';
-
+const tableName = 'user';
 export const getUser = async (id: number) => {
-    return await get<User>('user', id);
+    return await get<User>(tableName, id);
 };
 export const searchUser = async (user?: User) => {
-    return await select<User>('user', user);
+    return await select<User>(tableName, user);
 };
 export const createUser = async (user: User) => {
-    return await insert<User>('user', user);
+    return await insert<User>(tableName, user);
 };
 export const updateUser = async (user: User) => {
-    return await update<User>('user', user);
+    return await update<User>(tableName, user);
 };
 export const removeUser = async (id: number) => {
-    return await remove('user', id);
+    return await remove(tableName, id);
 };
