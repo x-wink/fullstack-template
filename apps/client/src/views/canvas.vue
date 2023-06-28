b3591eb6bb756b4eafbf6483c8e97c544b330669
向文可 <13202090601@163.com>
2023-06-28 18:58:13 +0800

    feat: 画布调试demo

--- apps/client/src/views/canvas.vue
+++ apps/client/src/views/canvas.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="canvas-container"><canvas id="cvs"></canvas></div>
+    <div class="canvas-container">
+        <canvas id="cvs"></canvas>
+        <div class="flex col-center form">
+            <label for="#rotate">旋转角度</label>
+            <input id="rotate" v-model="shapes.children[1].rotation" type="number" />
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
     import pic from '@/assets/test.png';
     import svg1 from '@/assets/1.svg?raw';
     import svg2 from '@/assets/2.svg?raw';
-    const shapes = {
+    const shapes = reactive({
         image: {
             src: pic,
         },
@@ -24,7 +30,7 @@
                 rotation: 45,
             },
         ],
-    };
+    });
     const createTransformMatrix = (angle = 0, scaleX = 1, scaleY = 1, flipHorizontal = false, flipVertical = false) => {
         angle = (angle * Math.PI) / 180;
         const cos = Math.cos(angle);
@@ -55,20 +61,30 @@
     };
 
     onMounted(async () => {
-        const cvs = document.getElementById('cvs') as HTMLCanvasElement;
         const svg = shapes.children[1];
-        const { width, height } = getTransformedSize(svg.width, svg.height, svg.rotation);
-        cvs.width = width;
-        cvs.height = height;
+        const cvs = document.getElementById('cvs') as HTMLCanvasElement;
         const ctx = cvs.getContext('2d')!;
+        watch(
+            shapes,
+            async () => {
+                const { width, height } = getTransformedSize(svg.width, svg.height, svg.rotation);
+                cvs.width = width;
+                cvs.height = height;
+                ctx.clearRect(0, 0, cvs.width, cvs.height);
 
-        const [centerX, centerY] = [cvs.width / 2, cvs.height / 2];
-        ctx.translate(centerX, centerY);
-        ctx.transform(...createTransformMatrix(svg.rotation));
-        ctx.translate(-centerX, -centerY);
+                const [centerX, centerY] = [cvs.width / 2, cvs.height / 2];
+                ctx.translate(centerX, centerY);
+                ctx.transform(...createTransformMatrix(svg.rotation));
+                ctx.translate(-centerX, -centerY);
 
-        const img = await loadImage(`data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg.svg)}`);
-        ctx.drawImage(img, (svg.width - width) / -2, (height - svg.height) / 2, svg.width, svg.height);
+                const img = await loadImage(`data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg.svg)}`);
+                ctx.drawImage(img, (svg.width - width) / -2, (height - svg.height) / 2, svg.width, svg.height);
+            },
+            {
+                immediate: true,
+                deep: true,
+            }
+        );
     });
 </script>
 
@@ -77,6 +93,18 @@
         background: #000;
         canvas {
             background: white;
+        }
+        .form {
+            margin-top: 30px;
+            font-size: 1.2em;
+            label {
+                margin-right: 20px;
+            }
+            input {
+                text-align: center;
+                font-size: 1.2em;
+                width: 100px;
+            }
         }
     }
 </style>
