d9883614a6f20d6b694ca2e006c7bbbde229f079
向文可 <13202090601@163.com>
2023-08-19 21:39:21 +0800

    refactor: docs

---
+++ apps/docs/examples/icon.md
@@ -0,0 +1,21 @@
+---
+title: 图标
+---
+
+# 图标
+
+`WINK UI` 从 `ElementPlus` 扒了所有图标过来使用
+
+# 使用方式
+
+```vue
+<Github />
+```
+
+```ts
+import { Github } from 'wink-ui';
+```
+
+# 图标列表
+
+<XIcon name="Logo" />
--- packages/docs/.vitepress/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-import { XIcon } from '@pkgs/ui';
-export default {
-    ...DefaultTheme,
-    enhanceApp: async ({ app, router, siteData }) => {
-        app.component('XIcon', XIcon);
-    },
-};
